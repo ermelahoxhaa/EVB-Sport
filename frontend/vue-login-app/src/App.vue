33138fcd8993a727b08823b061ba56b240c27f57
eh700997 <ermelahoxha13@gmail.com>
2025-04-01 21:51:53 +0200

    commiting changes

---
+++ frontend/vue-login-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <LoginComponent />
+  </div>
+</template>
+
+<script>
+import LoginComponent from './components/LoginComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LoginComponent
+  }
+};
+</script>
---
+++ frontend/vue-login-app/src/components/LoginComponent.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div class="login-container">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">Përdoruesi:</label>
+        <input type="text" v-model="username" id="username" required />
+      </div>
+      <div>
+        <label for="password">Fjalëkalimi:</label>
+        <input type="password" v-model="password" id="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+
+    <div v-if="errorMessage" class="error-message">
+      <p>{{ errorMessage }}</p>
+    </div>
+  </div>
+</template>
+
+
+
